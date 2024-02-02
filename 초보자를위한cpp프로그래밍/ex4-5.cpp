9aa235e4a3ab7d89a47f8bde3f413f40cbb20d8b
dmridmrir <122187770+dmridmrir@users.noreply.github.com>
2024-02-02 15:10:02 +0900

    Create ex4-5.cpp

---
+++ 초보자를위한cpp프로그래밍/ex4-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+float 형으로 433.4892와 16.3953사칙연산 수행
+*/
+int main(void) {
+	float a = 433.4892, b = 16.3953;
+
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+
+	return 0;
+}
