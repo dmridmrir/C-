1d69f52955aeb67d85d6a5e033b8672f2d78cf88
dmridmrir <122187770+dmridmrir@users.noreply.github.com>
2024-02-02 14:14:38 +0900

    Create ex3-4.cpp

---
+++ 초보자를위한cpp프로그래밍/ex3-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "신장과 체중을 입력하시오." << endl;
+	double h, w;
+	cin >> h;
+	cin >> w;
+	cout << "신장은 " << h << "cm입니다" << endl;
+	cout << "체중은 " << w << "kg입니다" << endl;
+
+	return 0;
+}
