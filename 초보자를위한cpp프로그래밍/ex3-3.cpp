8d5974483579bf50ba7a2ebead15cbf885729b98
dmridmrir <122187770+dmridmrir@users.noreply.github.com>
2024-02-02 14:12:45 +0900

    Create ex3-3.cpp

---
+++ 초보자를위한cpp프로그래밍/ex3-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	double pi;
+	cout << "원주율의 값은 얼마입니까?" << endl;
+	cin >> pi;
+	cout << "원주율의 값은 " << pi << "입니다." << endl;
+
+	return 0;
+}
