1d148c9559e0569d5be4a58daa1766918d8e4a99
dmridmrir <122187770+dmridmrir@users.noreply.github.com>
2024-02-02 14:18:12 +0900

    Create ex3-5.cpp

---
+++ 초보자를위한cpp프로그래밍/ex3-5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "알파벳의 마지막 문자는 무엇입니까" << endl;
+	char a;
+	cin >> a;
+	cout << "알파벳의 마지막 문자는 " << a << "입니다" << endl;
+
+	return 0;
+}
